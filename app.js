bd24ad2f568509e34c4c9d4acd501ced32a8dde9
Ye _Gaung_Swe <kvein.2000.apple@gmail.com>
2020-10-14 13:50:37 +0630

    get started

--- app.js
+++ app.js
@@ -1024,18 +1024,11 @@ function testDelete(sender_psid){
   callSendAPI(sender_psid, response);
 }
 
-const defaultReply = (sender_psid) => {
-  let response1 = {"text": "To test text reply, type 'text'"};
-  let response2 = {"text": "To test quick reply, type 'quick'"};
-  let response3 = {"text": "To test button reply, type 'button'"};   
-  let response4 = {"text": "To test webview, type 'webview'"};
-    callSend(sender_psid, response1).then(()=>{
-      return callSend(sender_psid, response2).then(()=>{
-        return callSend(sender_psid, response3).then(()=>{
-          return callSend(sender_psid, response4);
-        });
-      });
-  });  
+  const defaultReply = (sender_psid) => {
+  let response = getStarted(sender_psid);
+ callSend(sender_psid, response)
+}
+
 }
 
 const callSendAPI = (sender_psid, response) => {   
