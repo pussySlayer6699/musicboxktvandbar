8b2e1b2d156498bf4adb81be638572da92bc5804
Ye _Gaung_Swe <kvein.2000.apple@gmail.com>
2020-10-20 20:18:52 +0630

    reqsongs

--- app.js
+++ app.js
@@ -1210,6 +1210,7 @@ const confirmRequest = (sender_psid) => {
 
 const saveRequest = (arg, sender_psid) => {
   let data = arg;
+  data.status = "";
   data.created_on = new Date();
   db.collection('Song Requests').add(data).then((success)=>{
     console.log('SAVED', success);
